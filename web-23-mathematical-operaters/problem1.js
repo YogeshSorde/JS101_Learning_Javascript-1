693cc438e7546d466adaf516d463470edac441ca
Yogeshsorde <dineshsorde1997@gmail.com>
2022-10-14 23:22:42 +0530

    second depository

---
+++ web-23-mathematical-operaters/problem1.js
@@ -0,0 +1,6 @@
+let a=3;
+let b=2;
+console.log("sum",a+b);
+console.log("min",a-b);
+console.log("mult",a-b);
+console.log("div",a/b);
---
+++ web-23-mathematical-operaters/problem2.js
@@ -0,0 +1,5 @@
+let x="10";
+let y=2;
+let z=3;
+
+console.log(x+y+z);
